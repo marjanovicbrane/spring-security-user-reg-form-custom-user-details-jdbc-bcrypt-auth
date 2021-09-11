8ffb5eb7abbaaebe20b9d5862b503ac1a9a61af6
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-09-11 14:53:44 +0200

    I created a fourth config class SecurityWebApplicationInitializer.This class register SPRING SECURITY FILTERS.

---
+++ src/main/java/com/brane/security/user/reg/form/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,12 @@
+package com.brane.security.user.reg.form.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+//This class includes SPRING SECURITY FILTERS FOR WEB APP, THIS CLASS REGISTER SPRING SECURITY FILTERS.
+//SecurityWebApplicationInitializer class need to extend AbstractSecurityWebApplicationInitializer class,
+//if we want to enable filters.
+
+//THIS CLASS PROVIDES US SUPPORT FOR SECURITY INITIALIZATION, for INITIALIZATION SERVLET OBJECT.
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
