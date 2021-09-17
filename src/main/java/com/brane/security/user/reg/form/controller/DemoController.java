646a11488a81f2c0b24dfb2e59edf16f8e727d2a
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-09-17 14:40:58 +0200

    In controller class DemoController, I added a new method showLeaders.This method we are goin to use to show leaders.jsp page with this request mapping /leaders only for the users which have role MANAGER.

--- src/main/java/com/brane/security/user/reg/form/controller/DemoController.java
+++ src/main/java/com/brane/security/user/reg/form/controller/DemoController.java
@@ -8,10 +8,19 @@ public class DemoController {
 
 	
 	//Request mapping for root of our application to show home page when we are logged in.
+	//THIS CAN SEE ONLY USERS WITH EMPLOYEE ROLE
 	@GetMapping("/")
 	public String showHome() {
 		
 		return "home";
+	}
+	
+	
+	//Request mapping /leaders for MENAGER ROLE
+	@GetMapping("/leaders")
+	public String showLeaders() {
+			
+		return "leaders";
 	}
 	
 }
